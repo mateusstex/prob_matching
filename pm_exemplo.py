94b1d0580a2af99b39f6f01823a4a19cddd4ac68
Mateus Teixeira <mateusstex@gmail.com>
2023-01-18 16:14:53 -0300

    PM pronto
    Implementação do probability match realizada com sucesso, funcionando perfeitamente para o exemplo simples. Alguns ajustes ainda devem ser realizados, conforme comentários no código.

--- pm_exemplo.py
+++ pm_exemplo.py
@@ -11,15 +11,23 @@ chuvaMembro3 = np.array([ [0,1,5],[0,5,40],[0,1,7]], dtype='float')
 chuvaMembros = xr.DataArray([chuvaMembro1,chuvaMembro2,chuvaMembro3],
                             dims=['membro','lat','lon'],
                             name='chuva')
+nMembros, nLat, nLon = chuvaMembros.shape
 
-# campo médio (por enquanto soma!)
-chuvaMedia = chuvaMembros.sum(dim='membro')
+# campo médio
+chuvaMedia = chuvaMembros.mean(dim='membro')
 
-# nome das dimensões do campo médio
+print('Média do conjunto de previsões: (ANTES DO PROCESSAMENTO)')
+print(chuvaMedia)
+
+# nome das dimensões do campo médio, para ser usado na montagem
+# do dicionário usado na localização do dado no xr.DataArray
 dimsChuvaMedia = chuvaMedia.dims
 
 # arranjo para pesos do campo médio
-pesosCampoMedio = np.empty( chuvaMedia.shape )
+pesosCampoMedio = np.empty( chuvaMedia.shape, dtype=int )
+
+# arranjo para resultado final: PM
+prob_match = np.empty( chuvaMedia.shape )
 
 
 # loop que identifica a ordem dos valores no campo médio
@@ -32,7 +40,7 @@ pesosCampoMedio = np.empty( chuvaMedia.shape )
 #    2) atribui o peso ao arranjo de saída, na posição obtida
 #    3) atribui um valor negativo na entrada, na posição obtida
 #
-# a corrigir:
+# a CORRIGIR:
 #    1) não alterar a entrada!
 contaBuscaMax = 1
 pesoMaximo = chuvaMedia.size
@@ -64,8 +72,34 @@ while contaBuscaMax <= pesoMaximo:
    # incrementa o contador
    contaBuscaMax += 1
 
-# A FAZER:
-#     1) organizar em ordem decrescente os dados dos membros
-#     2) separá-los em grupos contendo a mesma quantidade de membros
-#     3) atribuir o valor mediano de cada grupo, do maior para o menor,
-#        em cada uma das posições identificadas anteriormente.
+# vetor com todos os dados de todos os membros do conjunto
+# de previsões, em ordem reversa dos seus valores
+dadosMembrosDecrescente = np.sort( np.ravel( chuvaMembros ) )[::-1]
+
+# loop para atribuir valores ao arranjo com resultado final
+posInicial = 0    # posição inicial do intervalo de valores a ser avaliado
+for item in np.arange( 1, chuvaMedia.size+1 ):
+   
+   # obtendo grupos com nMembros valores do vetor organizado
+   # em ordem decrescente e obtendo a mediana desses valores
+   novoValorCampo = np.median( dadosMembrosDecrescente[ posInicial : nMembros * item ] )
+
+   # atualizando campo final, com as medianas obtidas acima, de acordo com
+   # a posição dos maiores valores no campi médio, armazenados em 'pesosCampoMedio'
+   prob_match = np.where( pesosCampoMedio == item, novoValorCampo, prob_match)
+
+   # incrementando posição inicial do intervalo de valores a ser avaliado
+   posInicial += nMembros
+
+print('Média do conjunto de previsões: (APÓS PROCESSAMENTO)')
+print(chuvaMedia)
+print('PM do conjunto de previsões:')
+print(prob_match)
+
+''''
+CORREÇÕES:
+1) Não permitir a alteração do dado de entrada, ou seja, do campo médio de chuva
+   (VER COMENTÁRIOS ANTES LOOP WHILE)
+2) Tornar o PM do campo de chuva um xr.DataArray, com as mesmas dimensões e coords dos
+   campos originais (APÓS LOOP FOR)
+'''
