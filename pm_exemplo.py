b94416eea2bcea05403969e91ad2cd0124635e72
Mateus Teixeira <mateusstex@gmail.com>
2023-01-18 11:39:54 -0300

    Identificação das posições
    Identificação e atribuição de pesos aos valores do campo médio

--- pm_exemplo.py
+++ pm_exemplo.py
@@ -3,70 +3,69 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 # membros da previsão por conjunto (seriam lidos de arquivos)
-chuva_membro1 = np.array([ [0,0,1],[0,2,9],[0,5,50] ], dtype='float')
-chuva_membro2 = np.array([ [7,21,15],[17,60,20],[12,10,8]], dtype='float')
-chuva_membro3 = np.array([ [0,1,5],[0,5,40],[0,1,7]], dtype='float')
+chuvaMembro1 = np.array([ [0,0,1],[0,2,9],[0,5,50] ], dtype='float')
+chuvaMembro2 = np.array([ [7,21,15],[17,60,20],[12,10,8]], dtype='float')
+chuvaMembro3 = np.array([ [0,1,5],[0,5,40],[0,1,7]], dtype='float')
 
 # membros em um DataArray do xarray
-chuva_membros = xr.DataArray([chuva_membro1,chuva_membro2,chuva_membro3],
+chuvaMembros = xr.DataArray([chuvaMembro1,chuvaMembro2,chuvaMembro3],
                             dims=['membro','lat','lon'],
                             name='chuva')
 
 # campo médio (por enquanto soma!)
-chuva_media = chuva_membros.sum(dim='membro')
-
-# campo para vetor
-busca = 0
-#vetor_chuva_media = np.ravel(chuva_media)
-peso_maximo = chuva_media.size
+chuvaMedia = chuvaMembros.sum(dim='membro')
 
 # nome das dimensões do campo médio
-dimsChuvaMedia = chuva_media.dims
+dimsChuvaMedia = chuvaMedia.dims
 
-# índice 2D do maior valor do campo médio
-# retorna uma tupla com os índices da pos 
-posMaxChuvaMedia = np.unravel_index( 
-    np.argmax(chuva_media.values, 
-    axis=None), 
-    chuva_media.shape)
+# arranjo para pesos do campo médio
+pesosCampoMedio = np.empty( chuvaMedia.shape )
 
-# dicionário com o nome das dimensões do campo
-# e as coordenadas do máximo valor
-dictPosMaxChuvaMedia = dict( zip(dimsChuvaMedia,posMaxChuvaMedia) )
 
-##### A FAZER:
-'''
-1) criar DataArray que receberá os pesos, usando a coordenada obtida
-2) criar DataArray temp, com cópia dos dados da média, para alterar
-   os valores conforme a busca: buscar sempre o máximo valor, substituindo
-   o valor encontrado por np.nan para não afetar a busca seguinte
-3) atualizar o DataArray temp, colocando o np.nan no lugar do máximo 
-   encontrado
-4) inserir o peso na respectiva posição do máximo encontrado, no 
-   DataArray que receberá os pesos
-'''
+# loop que identifica a ordem dos valores no campo médio
+# entrada: campo médio, com N valores
+# saída: campo com pesos aos valores, sendo 1 para o maior
+#        e N para o menor valor.
+#
+# como funciona:
+#    1) busca pela posição do maior valor no dado de entrada
+#    2) atribui o peso ao arranjo de saída, na posição obtida
+#    3) atribui um valor negativo na entrada, na posição obtida
+#
+# a corrigir:
+#    1) não alterar a entrada!
+contaBuscaMax = 1
+pesoMaximo = chuvaMedia.size
 
-print(chuva_media.isel(  dictPosMaxChuvaMedia) ) 
-exit()
-while busca <= chuva_media.size:
-    posMaxChuva = np.unravel_index( np.argmax(chuva_media, axis=None), chuva_media.shape)
+while contaBuscaMax <= pesoMaximo:
     
+   # índice 2D do maior valor do campo médio:
+   # np.argmax retorna o índice do maior valor, dos dados
+   # organizados num vetor, e unravel_index retorna uma tupla
+   # com os índices nD, baseado na forma passada
+   posMaxChuvaMedia = np.unravel_index( 
+       np.argmax(chuvaMedia.values, axis=None), 
+       chuvaMedia.shape)
+   #print(posMaxChuvaMedia)
 
+   # dicionário com o nome das dimensões do campo
+   # e as coordenadas do máximo , para ser usado com 
+   # .loc, no xarray
+   dictPosMaxChuvaMedia = dict( zip(dimsChuvaMedia,posMaxChuvaMedia) )
+   #print(dictPosMaxChuvaMedia)
 
+   # atribuindo peso ao arranjo de pesos
+   pesosCampoMedio[ posMaxChuvaMedia ] = contaBuscaMax
 
+   # substituindo max encontrado por np.nan para
+   # seguir com a procura
+   chuvaMedia.loc[ dictPosMaxChuvaMedia ] = -999
 
-exit()
+   # incrementa o contador
+   contaBuscaMax += 1
 
-print(vetor_chuva_media)
-print(vetor_rank_chuva_media)
-exit()
-
-
-
-print(chuva_membros.shape)
-print(chuva_membros)
-print('******')
-print(chuva_media)
-
-print(np.reshape(np.argsort(np.ravel(chuva_media)),(3,3)))
-print(chuva_media.argsort())
+# A FAZER:
+#     1) organizar em ordem decrescente os dados dos membros
+#     2) separá-los em grupos contendo a mesma quantidade de membros
+#     3) atribuir o valor mediano de cada grupo, do maior para o menor,
+#        em cada uma das posições identificadas anteriormente.
