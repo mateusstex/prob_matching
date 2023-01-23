0997e9ba99af445ef30dfda29395a33cdaace477
Mateus Teixeira <mateusstex@gmail.com>
2023-01-23 17:54:02 -0300

    apenas um teste do GIT no windows

--- uso_funcao_PM.py
+++ uso_funcao_PM.py
@@ -7,7 +7,7 @@ chuvaMembro1 = np.array([ [0,0,1],[0,2,9],[0,5,50] ], dtype='float')
 chuvaMembro2 = np.array([ [7,21,15],[17,60,20],[12,10,8]], dtype='float')
 chuvaMembro3 = np.array([ [0,1,5],[0,5,40],[0,1,7]], dtype='float')
 
-# membros em um DataArray do xarray
+# membros em um DataArray do xarray (testando git no windows)
 chuvaMembros = xr.DataArray([chuvaMembro1,chuvaMembro2,chuvaMembro3],
                             dims=['membro','lat','lon'],
                             name='chuva')
